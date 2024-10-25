763a34c5fb58a72ff6f5b43e55ec4691f06ada06
Michał Kołacz <michal.kolacz45@gmail.com>
2024-10-25 17:28:34 +0200

    feat: add healthcheck and tcp check (#15)

---
+++ app/routes/resources+/healthcheck.tsx
@@ -0,0 +1,19 @@
+import { type LoaderFunctionArgs } from "@remix-run/node";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const host =
+    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+
+  try {
+    await fetch(`${new URL(request.url).protocol}${host}`, {
+      method: "HEAD",
+      headers: { "X-Healthcheck": "true" },
+    }).then((response) => {
+      if (!response.ok) return Promise.reject(response);
+    });
+    return new Response("Ok");
+  } catch (error: unknown) {
+    console.error("🛑 Healthcheck", { error });
+    return new Response("Error", { status: 500 });
+  }
+};
