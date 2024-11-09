9ac94e5532d9e0ed0b9f350581add9b87f9663af
Michał Kołacz <michal.kolacz45@gmail.com>
2024-11-09 18:32:08 +0100

    Fix/dev tools (#24)
    * fix: correct config for eslint, ts and prettier
    
    * fix: correct eslint and prettier

--- app/routes/blog+/$postTitle.tsx
+++ app/routes/blog+/$postTitle.tsx
@@ -1,13 +1,13 @@
 import { type MetaFunction, useParams } from "@remix-run/react";
 
-export const meta: MetaFunction = ({params}) => {
-  const postTitle=params.postTitle
+export const meta: MetaFunction = ({ params }) => {
+  const postTitle = params.postTitle;
 
   return [
     { title: `${postTitle}` },
     { name: "description", content: `Stille blog post ${postTitle}` },
   ];
-}
+};
 
 export default function BlogPost() {
   const params = useParams();
