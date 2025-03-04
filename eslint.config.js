8ca92f7b4e5966f758fb914d137216a74f811408
Michał Kołacz <michal.kolacz45@gmail.com>
2025-03-04 20:30:56 +0100

    feat: add Prettier ignore file and update ESLint configuration for improved directory handling

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,9 @@ import pluginReact from "eslint-plugin-react";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { ignores: ['.react-router/*', 'build/*', 'node_modules/*', 'server-build/*'] },
+  {
+    ignores: [".react-router/*", "build/*", "node_modules/*", "server-build/*"],
+  },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
