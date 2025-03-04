e4e7a1bf330d7675ba54b05d1406d06e0f5f90d2
Michał Kołacz <michal.kolacz45@gmail.com>
2025-03-04 20:28:03 +0100

    fix: separate ESLint ignores into distinct configuration entry

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,8 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], ignores: ['.react-router/*', 'build/*', 'node_modules/*', 'server-build/*'] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { ignores: ['.react-router/*', 'build/*', 'node_modules/*', 'server-build/*'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
