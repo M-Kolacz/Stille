43306f5fea1fb91f4b130c6ac7b3f10d0eb703d7
Michał Kołacz <michal.kolacz45@gmail.com>
2025-03-05 19:25:04 +0100

    Feat/fetch mdx file (#43)
    * feat: update article handling to read from MDX files and adjust ArticleCard props
    
    * feat: enhance blog loader to read multiple articles from directories and add new articles

--- app/routes/blog+/index.tsx
+++ app/routes/blog+/index.tsx
@@ -1,6 +1,8 @@
 import ArticleCard from "#app/components/article-card.tsx";
-import { getArticles } from "#app/utils/data.ts";
-import { type MetaFunction } from "react-router";
+import { useLoaderData, type MetaFunction } from "react-router";
+import { join } from "node:path";
+import { readdir, readFile } from "node:fs/promises";
+import { bundleMDX } from "mdx-bundler";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,8 +11,47 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const getFolderNames = async (path: string) => {
+  const entries = await readdir(path, { withFileTypes: true });
+  const folders = entries
+    .filter((entry) => entry.isDirectory())
+    .map((entry) => entry.name);
+  return folders;
+};
+
+export const loader = async () => {
+  const cwd = process.cwd();
+
+  const names = await getFolderNames(join(cwd, "posts"));
+
+  const articles = [];
+
+  for (const directoryName of names) {
+    const articlePath = join(cwd, "posts", directoryName, "index.mdx");
+    const articleContent = await readFile(articlePath, "utf-8");
+    const { frontmatter } = await bundleMDX<{
+      title: string;
+      date: string;
+      excerpt: string;
+    }>({
+      source: articleContent,
+    });
+    const convertedFrontmatter = {
+      ...frontmatter,
+      slug: frontmatter.title.toLowerCase().split(" ").join("-"),
+    };
+
+    articles.push(convertedFrontmatter);
+  }
+
+  return {
+    articles,
+  };
+};
+
 export default function HomePage() {
-  const articles = getArticles();
+  const { articles } = useLoaderData<typeof loader>();
+
   return (
     <>
       <div className="max-w-5xl mx-auto py-12">
@@ -20,7 +61,7 @@ export default function HomePage() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {articles.map((article) => (
-            <ArticleCard key={article.id} article={article} />
+            <ArticleCard key={article.title} article={article} />
           ))}
         </div>
       </div>
