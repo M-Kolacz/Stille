ab66c8f661cf1cbcb487004d3d1da93bf9e97344
Michał Kołacz <michal.kolacz45@gmail.com>
2025-03-10 20:10:56 +0100

    feat: add Codecov integration for bundle analysis and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
   build: {
     assetsInlineLimit: 0,
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    codecovVitePlugin({
+      enableBundleAnalysis: true,
+      bundleName: "michalkolacz.com",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
 });
